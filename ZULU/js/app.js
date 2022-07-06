b114a18d70bd833a44e39c10c060f1b54cb1dcdd
phunglq99 <phunglai1999@gmail.com>
2022-07-06 18:09:38 +0700

    7/6

---
+++ ZULU/js/app.js
@@ -0,0 +1,10 @@
+$('.client-list').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  //   dots: true,
+  prevArrow:
+    "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left icon-prevArrow' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right icon-nextArrow' aria-hidden='true'></i></button>",
+});
