13afa4d8bdca49fc1af62b8c9625bf42f0c4d4de
phunglq99 <phunglai1999@gmail.com>
2022-07-14 18:02:42 +0700

    7/14

--- PANDORA/js/app.js
+++ PANDORA/js/app.js
@@ -4,3 +4,16 @@ $('.banner').slick({
   autoplay: true,
   autoplaySpeed: 3000,
 });
+
+$('.trend-lists').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  centerMode: true,
+  variableWidth: true,
+  prevArrow:
+    "<button type='button' class='slick-prev pull-left trend-prev'><i class='fa-solid fa-angle-left' aria-hidden='true'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next pull-right trend-next'><i class='fa-solid fa-angle-right' aria-hidden='true'></i></button>",
+});
