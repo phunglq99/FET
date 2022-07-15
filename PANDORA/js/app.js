b9123d173f53035d3ccae6f6d04cead9a4db1937
phunglq99 <phunglai1999@gmail.com>
2022-07-15 18:07:09 +0700

    7/15

--- PANDORA/js/app.js
+++ PANDORA/js/app.js
@@ -17,3 +17,24 @@ $('.trend-lists').slick({
   nextArrow:
     "<button type='button' class='slick-next pull-right trend-next'><i class='fa-solid fa-angle-right' aria-hidden='true'></i></button>",
 });
+
+// input search
+var input = document.getElementById('input-search');
+var hisEL = document.querySelector('.search-lists');
+var itemEL = document.querySelectorAll('.search-item');
+var linkEL = document.querySelectorAll('.search-link');
+
+input.onkeyup = function () {
+  var filter = input.value.toUpperCase();
+
+  var lis = document.getElementsByTagName('li');
+  for (var i = 0; i < lis.length; i++) {
+    var name =
+      lis[i].getElementsByClassName('search-link')[0].innerHTML;
+    if (name.indexOf(filter) == 0) {
+      lis[i].style.display = 'block';
+    } else {
+      lis[i].style.display = 'none';
+    }
+  }
+};
